3e3e8944845ae1ade64a64c9bcea17666d72cdc4
LitherinStyke <li_styke@outlook.com>
2025-04-25 12:01:21 -0700

    added frame rate limiter

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    delta_time = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(F"Screen height: {SCREEN_HEIGHT}")
@@ -16,6 +18,7 @@ def main():
         screen.fill("black")
 
         pygame.display.flip()
+        delta_time = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
