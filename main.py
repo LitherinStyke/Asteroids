c24029f7e6ed3504b2ef6c06ffcab926ba6b1d41
LitherinStyke <li_styke@outlook.com>
2025-04-25 11:49:06 -0700

    Program can open and close now

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import pygame
-import constants
+from constants import *
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(F"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(F"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
